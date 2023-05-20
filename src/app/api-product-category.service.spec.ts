24478b5499e16d1a62bc9206ed574d9768a076e5
Shahin Zaman <shahinzaman@MacAir.local>
2023-05-20 12:30:34 +0600

    Added Unit Testing to ApiProductCategoryComponent & Service.

--- src/app/api-product-category.service.spec.ts
+++ src/app/api-product-category.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiProductCategoryService } from './api-product-category.service';
+import axios from 'axios';
 
 describe('ApiProductCategoryService', () => {
   let service: ApiProductCategoryService;
+  let axiosGetSpy: jasmine.Spy;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ApiProductCategoryService);
+    axiosGetSpy = spyOn(axios, 'get');
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call axios.get with the correct URL when getProductsByCategory is called', async () => {
+    const category = 'electronics';
+    const expectedResponse = [
+      { id: 1, title: 'Product 1', price: 10, description: 'Description 1', category: 'electronics', image: 'image1.jpg' },
+      { id: 2, title: 'Product 2', price: 20, description: 'Description 2', category: 'electronics', image: 'image2.jpg' }
+    ];
+
+    axiosGetSpy.and.returnValue(Promise.resolve({ data: expectedResponse }));
+
+    const result = await service.getProductsByCategory(category);
+
+    expect(axiosGetSpy).toHaveBeenCalledWith(`https://fakestoreapi.com/products/category/${category}`);
+    expect(result).toEqual(expectedResponse);
   });
 });
--- src/app/api-product-category/api-product-category.component.spec.ts
+++ src/app/api-product-category/api-product-category.component.spec.ts
@@ -1,23 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ApiProductCategoryComponent } from './api-product-category.component';
+import { ApiProductCategoryService } from '../api-product-category.service';
 
 describe('ApiProductCategoryComponent', () => {
   let component: ApiProductCategoryComponent;
   let fixture: ComponentFixture<ApiProductCategoryComponent>;
+  let apiProductCategoryService: ApiProductCategoryService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ApiProductCategoryComponent ]
+      declarations: [ApiProductCategoryComponent],
+      providers: [ApiProductCategoryService]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ApiProductCategoryComponent);
     component = fixture.componentInstance;
+    apiProductCategoryService = TestBed.inject(ApiProductCategoryService); // Use TestBed.inject() to retrieve the service instance
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set selectedCategory when onCategorySelected is called', () => {
+    const category = 'electronics';
+    component.onCategorySelected(category);
+    expect(component.selectedCategory).toBe(category);
+  });
+
+  it('should call getProductsByCategory when onCategorySelected is called', async () => {
+    const category = 'electronics';
+    const products = [
+      { id: 1, title: 'Product 1', price: 10, description: 'Description 1', category: 'electronics', image: 'image1.jpg' },
+      { id: 2, title: 'Product 2', price: 20, description: 'Description 2', category: 'electronics', image: 'image2.jpg' }
+    ];
+
+    spyOn(apiProductCategoryService, 'getProductsByCategory').and.returnValue(Promise.resolve(products));
+
+    await component.onCategorySelected(category);
+
+    expect(apiProductCategoryService.getProductsByCategory).toHaveBeenCalledWith(category);
+    expect(component.products).toEqual(products);
   });
 });

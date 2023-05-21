a46ec235014247f5adb5d9ef3e70a88580a9ebd3
Shahin Zaman <shahinzaman@MacAir.local>
2023-05-21 13:34:19 +0600

    Added Unit Testing to BuiltinPipesComponent.

--- src/app/builtin-pipes/builtin-pipes.component.spec.ts
+++ src/app/builtin-pipes/builtin-pipes.component.spec.ts
@@ -8,9 +8,9 @@ describe('BuiltinPipesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BuiltinPipesComponent ]
+      declarations: [BuiltinPipesComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(BuiltinPipesComponent);
     component = fixture.componentInstance;
@@ -20,4 +20,29 @@ describe('BuiltinPipesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update message$ when resend is called', () => {
+    const initialMessage$ = component.message$;
+
+    component.resend();
+
+    expect(component.message$).not.toBe(initialMessage$);
+  });
+
+  it('should update message$ with the expected values', () => {
+    const expectedMessages = ['You are my hero!', 'You are the best hero!', 'Will you be my hero?'];
+    let currentIndex = 0;
+
+    component.message$.subscribe(message => {
+      expect(message).toEqual(expectedMessages[currentIndex]);
+      currentIndex++;
+    });
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(currentIndex).toBe(expectedMessages.length);
+    });
+  });
+
+  // Add more unit tests as needed
 });

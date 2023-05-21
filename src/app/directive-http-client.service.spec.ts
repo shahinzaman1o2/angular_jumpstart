f302b89c80c7fd769b58f0e99632a3e4987bb039
Shahin Zaman <shahinzaman@MacAir.local>
2023-05-21 11:56:12 +0600

    Added Unit Testing to DirectiveHttpClientComponent & Service.

--- src/app/directive-http-client.service.spec.ts
+++ src/app/directive-http-client.service.spec.ts
@@ -1,19 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { DirectiveHttpClientService } from './directive-http-client.service';
+import { User } from './user.model';
 
 describe('DirectiveHttpClientService', () => {
   let service: DirectiveHttpClientService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule], // Import the HttpClientTestingModule
+      providers: [DirectiveHttpClientService]
     });
     service = TestBed.inject(DirectiveHttpClientService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve users from the API', () => {
+    const mockUsers: User[] = [
+      { id: 1, name: 'John', username: 'john01', email: 'john@example.com' },
+      { id: 2, name: 'Jane', username: 'jane02', email: 'jane@example.com' }
+    ];
+
+    service.getUsers().subscribe(users => {
+      expect(users).toEqual(mockUsers);
+    });
+
+    const req = httpMock.expectOne('https://jsonplaceholder.typicode.com/users');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers);
   });
 });
--- src/app/directive-http-client/directive-http-client.component.spec.ts
+++ src/app/directive-http-client/directive-http-client.component.spec.ts
@@ -3,23 +3,55 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { DirectiveHttpClientComponent } from './directive-http-client.component';
 import { FormsModule } from '@angular/forms';
+import { DirectiveHttpClientService } from '../directive-http-client.service';
+import { of } from 'rxjs';
 
 describe('DirectiveHttpClientComponent', () => {
   let component: DirectiveHttpClientComponent;
   let fixture: ComponentFixture<DirectiveHttpClientComponent>;
+  let directiveHttpClientService: DirectiveHttpClientService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DirectiveHttpClientComponent],
       imports: [FormsModule, HttpClientTestingModule], // Import the HttpClientTestingModule
+      providers: [DirectiveHttpClientService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DirectiveHttpClientComponent);
     component = fixture.componentInstance;
+    directiveHttpClientService = TestBed.inject(DirectiveHttpClientService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch users on initialization', () => {
+    const mockUsers = [
+      { id: 1, name: 'John', username: 'john01', email: 'john@example.com' },
+      { id: 2, name: 'Jane', username: 'jane02', email: 'jane@example.com' }
+    ];
+    spyOn(directiveHttpClientService, 'getUsers').and.returnValue(of(mockUsers));
+
+    component.ngOnInit();
+
+    expect(component.users).toEqual(mockUsers);
+    expect(directiveHttpClientService.getUsers).toHaveBeenCalled();
+  });
+
+  it('should filter users based on the filterValue', () => {
+    component.users = [
+      { id: 1, name: 'John', username: 'john01', email: 'john@example.com' },
+      { id: 2, name: 'Jane', username: 'jane02', email: 'jane@example.com' },
+      { id: 3, name: 'David', username: 'david03', email: 'david@example.com' }
+    ];
+    component.filterValue = 'john';
+
+    const filteredUsers = component.filteredUsers;
+
+    expect(filteredUsers.length).toBe(1);
+    expect(filteredUsers[0].name).toBe('John');
   });
 });

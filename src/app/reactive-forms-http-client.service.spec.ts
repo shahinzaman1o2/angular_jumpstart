eb5e4cfc68c594a195b50d632bd712c8a4932c6e
Shahin Zaman <shahinzaman@MacAir.local>
2023-05-21 13:30:28 +0600

    Added Unit Testing to ReactiveFormsHttpClientComponent & Service.

--- src/app/reactive-forms-http-client.service.spec.ts
+++ src/app/reactive-forms-http-client.service.spec.ts
@@ -1,19 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ReactiveFormsHttpClientService } from './reactive-forms-http-client.service';
+import { User } from './user.model';
 
 describe('ReactiveFormsHttpClientService', () => {
   let service: ReactiveFormsHttpClientService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule], // Add the HttpClientTestingModule
+      providers: [ReactiveFormsHttpClientService]
     });
     service = TestBed.inject(ReactiveFormsHttpClientService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve users', () => {
+    const mockUsers: User[] = [
+      { id: 1, name: 'John', username: 'john123', email: 'john@example.com' },
+      { id: 2, name: 'Jane', username: 'jane456', email: 'jane@example.com' }
+    ];
+
+    service.getUsers().subscribe(users => {
+      expect(users.length).toBe(2);
+      expect(users).toEqual(mockUsers);
+    });
+
+    const req = httpMock.expectOne('https://jsonplaceholder.typicode.com/users');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers);
   });
 });
--- src/app/reactive-forms-http-client/reactive-forms-http-client.component.spec.ts
+++ src/app/reactive-forms-http-client/reactive-forms-http-client.component.spec.ts
@@ -1,25 +1,86 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ReactiveFormsHttpClientComponent } from './reactive-forms-http-client.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsHttpClientService } from '../reactive-forms-http-client.service';
+import { User } from '../user.model';
+import { of } from 'rxjs';
 
 describe('ReactiveFormsHttpClientComponent', () => {
   let component: ReactiveFormsHttpClientComponent;
   let fixture: ComponentFixture<ReactiveFormsHttpClientComponent>;
+  let service: ReactiveFormsHttpClientService;
+  let httpMock: HttpTestingController;
+
+  const mockUsers: User[] = [
+    { id: 1, name: 'John', username: 'john123', email: 'john@example.com' }
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ReactiveFormsHttpClientComponent],
-      imports: [HttpClientTestingModule, ReactiveFormsModule], // Import the HttpClientTestingModule
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      providers: [ReactiveFormsHttpClientService, FormBuilder]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ReactiveFormsHttpClientComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(ReactiveFormsHttpClientService);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    // Mock the getUsers request
+    spyOn(service, 'getUsers').and.returnValue(of(mockUsers));
+
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should filter users based on the filter value', () => {
+    component.users = [
+      { id: 1, name: 'John Doe', username: 'johndoe', email: 'johndoe@example.com' },
+      { id: 2, name: 'Jane Smith', username: 'janesmith', email: 'janesmith@example.com' }
+    ];
+
+    component.filterForm.get('filter')?.setValue('joh');
+    const filteredUsers = component.filteredUsers;
+
+    expect(filteredUsers.length).toBe(1);
+    expect(filteredUsers[0].name).toBe('John Doe');
+  });
+
+  it('should submit the form when valid', () => {
+    const validFormData = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '12345678'
+    };
+    component.formGroup.setValue(validFormData);
+    const consoleSpy = spyOn(console, 'log');
+
+    component.onSubmit();
+
+    expect(consoleSpy).toHaveBeenCalledWith('Form submitted:', validFormData);
+  });
+
+  it('should not submit the form when invalid', () => {
+    const invalidFormData = {
+      name: '',
+      email: 'invalidemail',
+      password: '123'
+    };
+    component.formGroup.setValue(invalidFormData);
+    const consoleSpy = spyOn(console, 'log');
+
+    component.onSubmit();
+
+    expect(consoleSpy).not.toHaveBeenCalled();
   });
 });
